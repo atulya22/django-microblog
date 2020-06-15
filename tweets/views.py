ee11499038f24b45740895d4318af8696924649d
atulya22 <atulya.shetty@hotmail.com>
2020-06-15 09:52:54 -0400

    Add home and base view templates and setup endpoints for tweets api

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
+from django.http import HttpResponse, JsonResponse
+
+from .models import Tweets
 
 # Create your views here.
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "tweets/home.html")
+
+def tweet_detail_view(request, tweet_id, *arg, **kwargs):
+    data = {
+        "id": tweet_id,
+        }
+    status = 200    
+    try:
+        tweet = Tweets.objects.get(id=tweet_id)
+        data['content'] = tweet.content
+    except:
+        data['message'] = "Tweet not found"
+        status = 404
+
+    return JsonResponse(data, status= status)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+        REST API VIEW
+        CONSUMED BY THE FRONTEND
+        RETURNS JSON DATA
+    """
+    tweet_qs = Tweets.objects.all()
+    tweet_list = [{ "id": tweet.id, "content": tweet.content } for tweet in tweet_qs]
+    data = {
+        "response": tweet_list
+    }
+
+    return JsonResponse(data)

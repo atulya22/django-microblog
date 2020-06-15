668da37b6651ef29a4a96648922e915f3c68b200
atulya22 <atulya.shetty@hotmail.com>
2020-06-15 16:45:21 -0400

    Add serialize function on the tweet model Add JS error handling logic on the frontend

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,38 @@
-from django.shortcuts import render, Http404
+from django.shortcuts import render, Http404, redirect
 from django.http import HttpResponse, JsonResponse
+from django.utils.http import is_safe_url
+from django.conf import settings
+
 import random 
 from .models import Tweets
+from .forms import TweetForm
 
-# Create your views here.
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 
 def home_view(request, *args, **kwargs):
     return render(request, "main/home.html")
+
+
+def tweet_create_view(request, *args, **kwargs):
+    print("Ajax request", request.is_ajax())
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
+        if next_url is not None and is_safe_url(next_url, allowed_hosts=ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+
+    return render(request, "components/form.html", context={"form": form})
 
 def tweet_detail_view(request, tweet_id, *arg, **kwargs):
     data = {
@@ -31,7 +56,7 @@ def tweet_list_view(request, *args, **kwargs):
         RETURNS JSON DATA
     """
     tweet_qs = Tweets.objects.all()
-    tweet_list = [{ "id": tweet.id, "content": tweet.content, "likes": random.randint(0, 1000) } for tweet in tweet_qs]
+    tweet_list = [tweet.serialize() for tweet in tweet_qs]
     data = {
         "isUser": False,
         "response": tweet_list

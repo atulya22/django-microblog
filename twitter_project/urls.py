032fdce6ab6f32fc454ba335d00dbe9c53164100
atulya22 <atulya.shetty@hotmail.com>
2020-06-16 23:18:19 -0400

    More cleansnup - Remove old JS and django code

--- twitter_project/urls.py
+++ twitter_project/urls.py
@@ -33,11 +33,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
     path('react/', TemplateView.as_view(template_name='react_django.html')),
-    path('tweets/<int:tweet_id>/', tweet_detail_view),
-    path('tweets/', tweet_list_view),
-    path('create-tweet/', tweet_create_view),
-    # path('api/tweets/<int:tweet_id>/delete/', tweet_delete_view),
-    # path('api/tweets/action/', tweet_action_view),
     path('api/tweets/', include('tweets.urls'))
 ]
 

6d40f9187c4aada44d7245efeddd41a430b72868
atulya22 <atulya.shetty@hotmail.com>
2020-06-17 15:57:18 -0400

    Login, logout and register logic

--- twitter_project/urls.py
+++ twitter_project/urls.py
@@ -26,9 +26,18 @@ from tweets.views import (
     tweets_profile_view
 )
 
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tweets_list_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
     path('profile/<str:username>', tweets_profile_view),
     path('api/tweets/', include('tweets.api.urls'))

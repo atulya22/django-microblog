a3f0713ceed85ae27c332a36038497048b9f5108
atulya22 <atulya.shetty@hotmail.com>
2020-06-17 13:34:59 -0400

    Create api folder to manage django apis Integrate lastest react build into django

--- tweets/views.py
+++ tweets/views.py
@@ -4,112 +4,19 @@ from django.http import HttpResponse, JsonResponse
 from django.utils.http import is_safe_url
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
-# DRF
-from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication
-from rest_framework.decorators import (api_view,
-                                       permission_classes,
-                                       authentication_classes)
-from rest_framework.permissions import IsAuthenticated
-
-# General Python
-import random
-
-# Internal
-from .serializers import (
-    TweetSerializer,
-    TweetActionSerializer,
-    TweetCreateSerializer
-)
-
-from .models import Tweets
-from .forms import TweetForm
-
-ALLOWED_HOSTS = settings.ALLOWED_HOSTS
-
 
 def home_view(request, *args, **kwargs):
     return render(request, "main/home.html")
 
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetCreateSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save(user=request.user)
-        return Response(serializer.data, status=201)
-    return Response({}, status=400)
+def tweets_list_view(request, *args, **kwargs):
+    return render(request, "tweets/list.html")
 
 
-@api_view(['GET'])
-def tweet_list_view(request, *args, **kwargs):
-    qs = Tweets.objects.all()
-    username = request.GET.get('username')
-    if username is not None:
-        qs = qs.filter(user__username__iexact=username)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+def tweets_detail_view(request, tweet_id, *args, **kwargs):
+    return render(request, "tweets/detail.html",
+                  context={"tweet_id": tweet_id})
 
 
-@api_view(['GET'])
-def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    qs = Tweets.objects.filter(id=tweet_id)
-    if not qs.exists():
-        return Response({}, status=404)
-    obj = qs.first()
-    serializer = TweetSerializer(obj)
-    return Response(serializer.data)
-
-
-@api_view(['DELETE', 'POST'])
-@permission_classes([IsAuthenticated])
-def tweet_delete_view(request, tweet_id, *args, **kwargs):
-    qs = Tweets.objects.filter(id=tweet_id)
-    if not qs.exists():
-        return Response({}, status=404)
-
-    qs = qs.filter(user=request.user)
-    if not qs.exists():
-        return Response({"message": "You cannot delete this tweet"},
-                        status=401)
-
-    obj = qs.first()
-    obj.delete()
-    return Response({"message": "Tweet was deleted"}, status=200)
-
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def tweet_action_view(request, *args, **kwargs):
-    '''
-        Actions view: like, unlike and retweet actions
-        id required
-    '''
-
-    serializer = TweetActionSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        data = serializer.validated_data
-        tweet_id = data.get('id')
-        action = data.get('action')
-        content = data.get('content')
-
-        qs = Tweets.objects.filter(id=tweet_id)
-        if not qs.exists():
-            return Response({}, status=404)
-
-        obj = qs.first()
-        if action == 'like':
-            obj.likes.add(request.user)
-            serializer = TweetSerializer(obj)
-            return Response(serializer.data, status=200)
-        elif action == 'unlike':
-            obj.likes.remove(request.user)
-            serializer = TweetSerializer(obj)
-            return Response(serializer.data, status=200)
-        elif action == 'retweet':
-            new_tweet = Tweets.objects.create(user=request.user, content=content, parent=obj)
-            serializer = TweetSerializer(new_tweet)
-            return Response(serializer.data, status=201)
-
-    return Response({}, status=200)
+def tweets_profile_view(request, username, *args, **kwargs):
+    return render(request, "tweets/profile.html",
+                  context={"profile_username": username})
